684d222458b76cff37d2463912d5520c5892f666
jwong65 <jason.wong6507@gmail.com>
2023-04-20 19:47:18 -0400

    Added the migration and updated the 2nd blueprint

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask 
+from flask_migrate import Migrate
+
 
 def create_app(): 
     app = Flask(__name__)
@@ -11,6 +13,18 @@ def create_app():
     from . import pet
     app.register_blueprint(pet.bp)
 
+    # Registering fact blueprint
+    from . import fact
+    app.register_blueprint(fact.bp)
+
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
+    
+    # Configuring the database
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
 
     return app
--- petfax/pet.py
+++ petfax/pet.py
@@ -7,14 +7,15 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 
 @bp.route('/')
 def index():
-    return render_template('index.html', pets=pets)
+    return render_template('/pets/index.html', pets=pets)
 
 # https://flask.palletsprojects.com/en/2.0.x/quickstart/#variable-rules 
 @bp.route('/<int:id>')
 def show_pet(id):
     pet = pets[id-1]
-    return render_template('show.html', pet =pet)
+    return render_template('/pets/show.html', pet =pet)
 
-@bp.route('/facts')
-def facts():
-    return render_template('facts.html')
+# @bp.route('/facts')
+# def facts():
+#     return render_template('facts.html')
+    
