b05aa3845a801ee124ad89650918727ffb8c938a
jwong65 <jason.wong6507@gmail.com>
2023-04-20 15:48:00 -0400

    Added template

--- petfax/pet.py
+++ petfax/pet.py
@@ -8,3 +8,8 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 @bp.route('/')
 def index():
     return render_template('index.html', pets=pets)
+
+# https://flask.palletsprojects.com/en/2.0.x/quickstart/#variable-rules 
+@bp.route('/<pet_id>')
+def show_pet(pet_id):
+    return render_template('show.html', pets =pets)
